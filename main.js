bfa213c6a09ce3330646b5374acc3a6155a5e548
Shehabmohamed86 <shehabmohamed8688@gmail.com>
2022-08-31 23:02:23 -0400

    faq

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 const questions = document.querySelectorAll('.flex')
 const answers = document.querySelectorAll('p')
-const h5s   = document.querySelectorAll('h5')
+const h5s  = document.querySelectorAll('h5')
 
 
-
-questions.forEach(question=>{
-  question.addEventListener('click',isActive)
+questions.forEach((question,i)=>{
+ question.addEventListener('click',()=>isActive(i))
 })
 
 
-function isActive() {
- answers.forEach(answer=>{
-  answer.classList.toggle('is-active')
- })
 
- h5s.forEach(h5=>{
-  h5.classList.toggle('larger')
- })}
+function isActive(i){
+  answers.forEach((answer,j)=>{
+    if(i !== j) return  answer.classList.remove('is-active')
 
+   answers[i].classList.toggle('is-active')
+  
+  h5s.forEach((h5,j)=>{
+    if(i!==j) return  h5.classList.remove('larger')
+    
+  })
+  h5s[i].classList.toggle('larger')
+ 
+  })
 
-
+}
